e9d757f17352412805e3d2d252640437419a3326
ana-b-c <ana-belen.correa@epitech.eu>
2026-09-19 20:42:53 +0200

    feat: add undo and redo history

--- src/components/EditorLayout/EditorLayout.tsx
+++ src/components/EditorLayout/EditorLayout.tsx
@@ -6,33 +6,144 @@
  */
 
 import { useState } from 'react'
+
 import CanvasWorkspace from '../../canvas/CanvasWorkspace/CanvasWorkspace'
+import { useBrushOptions } from '../../hooks/useBrushOptions'
+import { useEraserOptions } from '../../hooks/useEraserOptions'
+import { useHistory } from '../../hooks/useHistory'
 import { useRasterDocument } from '../../hooks/useRasterDocument'
+import { useSelection } from '../../hooks/useSelection'
+import { useToolManager } from '../../hooks/useToolManager'
+import { useZoom } from '../../hooks/useZoom'
 import LayersPanel from '../LayersPanel/LayersPanel'
 import MenuBar from '../MenuBar/MenuBar'
 import NewDocumentDialog from '../NewDocumentDialog/NewDocumentDialog'
 import StatusBar from '../StatusBar/StatusBar'
 import Toolbar from '../Toolbar/Toolbar'
 import ToolOptions from '../ToolOptions/ToolOptions'
+
 import './EditorLayout.css'
-import { useZoom } from '../../hooks/useZoom'
 
 function EditorLayout() {
   const [isNewDocumentOpen, setIsNewDocumentOpen] = useState(false)
-  const { document, error, createDocument, openDocument } = useRasterDocument()
+
+  const {
+    document,
+    error,
+    createDocument,
+    openDocument,
+    updateDocumentImageData,
+    cropDocument,
+    replaceDocument,
+  } = useRasterDocument()
+
   const { zoom, zoomIn, zoomOut } = useZoom()
+
+  const { activeTool, selectTool } = useToolManager()
+
+  const { brushOptions, setBrushSize, setBrushColor } = useBrushOptions()
+
+  const { eraserOptions, setEraserSize } = useEraserOptions()
+
+  const { selection, setSelection, clearSelection } = useSelection()
+
+  const { pushHistory, undo, redo, clearHistory, canUndo, canRedo } = useHistory()
+
+  const handleCreateDocument = (width: number, height: number): void => {
+    createDocument(width, height)
+    clearSelection()
+    clearHistory()
+    setIsNewDocumentOpen(false)
+  }
+
+  const handleEditStart = (): void => {
+    if (!document) {
+      return
+    }
+
+    pushHistory(document)
+  }
+
+  const handleCrop = (): void => {
+    if (!document || !selection) {
+      return
+    }
+
+    pushHistory(document)
+    cropDocument(selection)
+    clearSelection()
+  }
+
+  const handleUndo = (): void => {
+    if (!document) {
+      return
+    }
+
+    const previousDocument = undo(document)
+
+    if (!previousDocument) {
+      return
+    }
+
+    replaceDocument(previousDocument)
+    clearSelection()
+  }
+
+  const handleRedo = (): void => {
+    if (!document) {
+      return
+    }
+
+    const nextDocument = redo(document)
+
+    if (!nextDocument) {
+      return
+    }
+
+    replaceDocument(nextDocument)
+    clearSelection()
+  }
 
   return (
     <div className="editor-layout">
-      <MenuBar onNewDocument={() => setIsNewDocumentOpen(true)} onOpenImage={openDocument} />
+      <MenuBar
+        onNewDocument={() => setIsNewDocumentOpen(true)}
+        onOpenImage={openDocument}
+        onUndo={handleUndo}
+        onRedo={handleRedo}
+        canUndo={canUndo}
+        canRedo={canRedo}
+      />
 
-      <ToolOptions />
+      <ToolOptions
+        activeTool={activeTool}
+        brushOptions={brushOptions}
+        eraserOptions={eraserOptions}
+        onBrushSizeChange={setBrushSize}
+        onBrushColorChange={setBrushColor}
+        onEraserSizeChange={setEraserSize}
+        hasSelection={selection !== null && selection.width > 0 && selection.height > 0}
+        onCrop={handleCrop}
+      />
 
       {error && <p className="editor-layout__error">{error}</p>}
 
       <div className="editor-layout__workspace">
-        <Toolbar />
-        <CanvasWorkspace document={document} zoom={zoom} />
+        <Toolbar activeTool={activeTool} onToolSelect={selectTool} />
+
+        <CanvasWorkspace
+          document={document}
+          zoom={zoom}
+          activeTool={activeTool}
+          brushOptions={brushOptions}
+          eraserOptions={eraserOptions}
+          selection={selection}
+          onImageDataChange={updateDocumentImageData}
+          onForegroundColorChange={setBrushColor}
+          onSelectionChange={setSelection}
+          onEditStart={handleEditStart}
+        />
+
         <LayersPanel />
       </div>
 
@@ -46,7 +157,7 @@ function EditorLayout() {
       <NewDocumentDialog
         isOpen={isNewDocumentOpen}
         onClose={() => setIsNewDocumentOpen(false)}
-        onCreate={createDocument}
+        onCreate={handleCreateDocument}
       />
     </div>
   )
